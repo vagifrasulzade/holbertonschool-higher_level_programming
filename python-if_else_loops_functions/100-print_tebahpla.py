5a8fda57acea81bfb507323c696a0717d54d905a
vagifrasulzade <vaqufresulzade@gmail.com>
2025-11-25 09:30:33 +0000

    task 13

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
-# !/usr/bin/python3
-for i in range(122, 96 , -1):
-if(122 - i) % 2 == 0:
+#!/usr/bin/python3
+for i in range(122, 96, -1):
+    if (122 - i) % 2 == 0:
         print("{}".format(chr(i)), end="")
     else:
         print("{}".format(chr(i - 32)), end="")
