9b709fcc4eaadb50300cabf2d0bacb18dbc3c685
vagifrasulzade <vaqufresulzade@gmail.com>
2025-12-01 14:18:02 +0000

    task 4

--- python-import_modules/tmp/4-hidden_discovery.py
+++ python-import_modules/tmp/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 import hidden_4
 if __name__ == "__main__":
     names = dir(hidden_4)
-    for n in names:
-        if n[:2] != "__":
-            print(n)
+    for i in names:
+        if i[:2] != "__":
+            print(i)ls
+            
