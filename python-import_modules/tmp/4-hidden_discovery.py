7d4878bcf4c1d3ed6e161d331a4bf294963b03f4
vagifrasulzade <vaqufresulzade@gmail.com>
2025-12-01 17:49:47 +0000

    task 4

--- python-import_modules/tmp/4-hidden_discovery.py
+++ python-import_modules/tmp/4-hidden_discovery.py
@@ -5,3 +5,13 @@ if __name__ == "__main__":
     for i in names:
         if i[:2] != "__":
             print(i)
+
+
+
+
+
+#example
+# Linux 
+# mkdir /tmp
+# cd /tmp
+# curl -Lso hidden_4.pyc "https://github.com/hs-hq/0x02.py/raw/main/hidden_4.pyc"
