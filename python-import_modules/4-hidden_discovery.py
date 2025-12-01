abb25b0fcf03e34c20460a89feee4f397b7ec126
vagifrasulzade <vaqufresulzade@gmail.com>
2025-12-01 13:45:05 +0000

    Task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-
 if __name__ == "__main__":
-
-    for name in sorted(dir(hidden_4)):
-        if not name.startswith("__"):
-            print(name)
-#terminal
-# curl -Lso "hidden_4.pyc" "https://github.com/hs-hq/0x02.py/raw/main/hidden_4.pyc"
+    names = dir(hidden_4)
+    for n in names:
+        if n[:2] != "__":
+            print(n)
