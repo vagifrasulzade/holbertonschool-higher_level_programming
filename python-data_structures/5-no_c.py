4d68f2ce7a69e7ca6a9ec1988cad05f28a99af39
vagifrasulzade <vaqufresulzade@gmail.com>
2025-12-03 13:09:29 +0000

    Task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return my_string.replace('c', '').replace('C', '')
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
