9cf486d1818eed1822ce4c719a91b01a500e05b1
vagifrasulzade <vaqufresulzade@gmail.com>
2025-12-05 10:12:17 +0000

    Task 6

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
-
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
