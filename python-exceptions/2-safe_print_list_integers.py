9ad937df47a39f905ddd30dadb3aaa617170b5d6
vagifrasulzade <vaqufresulzade@gmail.com>
2025-12-12 13:03:07 +0000

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    for i in range(x):
+    final_sentence = ''
+    accumulator = 0
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError, IndexError):
-            continue
-    print()
-    return count
+            accumulator += 1
+        except (ValueError, TypeError):
+            pass
+    print(final_sentence)
+    return accumulator
