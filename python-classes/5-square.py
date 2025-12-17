7d85f452ccec632aa1533720d9a8450aa6b80eee
vagifrasulzade <vaqufresulzade@gmail.com>
2025-12-17 14:02:38 +0000

    Task 6

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -10,12 +10,10 @@ class Square:
 
     @property
     def size(self):
-        """Retrieve the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size of the square with validation."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -23,11 +21,9 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Return the area of the square."""
         return self.__size ** 2
 
     def my_print(self):
-        """Print the square using # characters."""
         if self.__size == 0:
             print()
             return
