67d30091af1af80b546b4203843fd32a00d58693
unknown <vaqufresulzade@gmail.com>
2025-12-23 14:20:47 +0400

    Task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines Rectangle class"""
+"""Module that defines Square class"""
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle class"""
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
