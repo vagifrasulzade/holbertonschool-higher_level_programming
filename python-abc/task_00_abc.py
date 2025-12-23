b321127bbe9738c1f7a1246ce1ab3586b0665f50
unknown <vaqufresulzade@gmail.com>
2025-12-23 15:05:15 +0400

    Task 0

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+
+
+"""Abstract base class for animals"""
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    def sound(self):
+        return "Woof"
+
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
