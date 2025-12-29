15cc18ce3b02e0909aa6b63542e8e7a1e16e6cde
unknown <vaqufresulzade@gmail.com>
2025-12-29 19:34:00 +0400

    Task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Defines a student."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
