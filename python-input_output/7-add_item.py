158d610953fa8faf8589972a52929610f11204f3
unknown <vaqufresulzade@gmail.com>
2025-12-29 19:12:25 +0400

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Module that adds all arguments to a Python list, and then save them to a file."""
+"""Script that adds all arguments to a Python list,
+ and then save them to a file.
+"""
 import sys
 
 save_to_json_file = __import__(
