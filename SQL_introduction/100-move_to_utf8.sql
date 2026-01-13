5b9911592cbd41fcbb6fff9912942d8dc100d1cd
unknown <vaqufresulzade@gmail.com>
2026-01-13 17:15:10 +0400

    Task 100

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,9 @@
 -- Convert database hbtn_0c_0 to UTF8MB4
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- Select the database
+USE hbtn_0c_0;
+
 -- Convert table first_table to UTF8MB4
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
