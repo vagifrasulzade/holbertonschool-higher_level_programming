e55d4b894e0c349f63725a1eaacd659b589a8e34
unknown <vaqufresulzade@gmail.com>
2026-01-13 17:19:34 +0400

    Task 18

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Temperatures #0
-SELECT 'city', AVG('value') AS avg_temp
-FROM 'temperatures'
-GROUP BY 'city'
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
 ORDER BY avg_temp DESC;
