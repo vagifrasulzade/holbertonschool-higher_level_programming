e8ef3a8301b25aeb95c551b5ceebdec3bc31379a
unknown <vaqufresulzade@gmail.com>
2026-01-25 19:39:10 +0400

    mapping

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
 """This script lists all states from the database hbtn_0e_0_usa"""
-
-import sys
-import pymysql
-pymysql.install_as_MySQLdb()  # Windows-da MySQLdb əvəzinə PyMySQL istifadə
 import MySQLdb
+import sys
 
 
 def connection():
-    """Connects to the database and prints all states sorted by id."""
     usrnm = sys.argv[1]
     psswrd = sys.argv[2]
     db_name = sys.argv[3]
 
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=usrnm,
-        passwd=psswrd,
-        db=db_name
-    )
+         host='localhost',
+         port=3306,
+         user=usrnm,
+         password=psswrd,
+         database=db_name)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+
+    sql = "SELECT * FROM states ORDER BY states.id ASC;"
+    cursor.execute(sql)
+
     rows = cursor.fetchall()
     for row in rows:
         print(row)
