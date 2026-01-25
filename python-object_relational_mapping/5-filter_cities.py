27b618d37152bdb4278bfe43eb75b3ce44486a6d
vagifrasulzade <vaqufresulzade@gmail.com>
2026-01-25 17:12:36 +0000

    .

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,45 +1,40 @@
 #!/usr/bin/python3
-"""This script lists all cities of a given
- state from the database hbtn_0e_0_usa
 """
+First ORM
+"""
+
 import MySQLdb
 import sys
 
-
-def connection():
-    usrnm = sys.argv[1]
-    psswrd = sys.argv[2]
-    db_name = sys.argv[3]
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
     state_name = sys.argv[4]
 
     db = MySQLdb.connect(
-        host='localhost',
+        host="localhost",
         port=3306,
-        user=usrnm,
-        password=psswrd,
-        database=db_name)
+        user=user,
+        passwd=password,
+        db=database
+    )
 
     cursor = db.cursor()
 
-    sql = ("SELECT cities.name "
-           "FROM cities "
-           "JOIN states ON cities.state_id = states.id "
-           "WHERE states.name = %s "
-           "ORDER BY cities.id ASC;")
-
-    cursor.execute(sql, (state_name, ))
+    cursor.execute(
+        """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """,
+        (state_name,)
+    )
 
     rows = cursor.fetchall()
-
-    cities = []
-    for row in rows:
-        cities.append(row[0])
-
-    print(", ".join(cities))
+    print(", ".join(row[0] for row in rows))
 
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    connection()
