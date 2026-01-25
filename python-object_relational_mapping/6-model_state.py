8a90e34cbe091f8a9ddfe789d4399f4b2d0d4590
vagifrasulzade <vaqufresulzade@gmail.com>
2026-01-25 17:14:21 +0000

    .

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
 """
-This script creates the State
-"California" with the City "San Francisco"
-from the database hbtn_0e_6_usa
+First ORM
 """
 
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-from sqlalchemy import (create_engine)
-
 if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            user, password, database
+        )
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
