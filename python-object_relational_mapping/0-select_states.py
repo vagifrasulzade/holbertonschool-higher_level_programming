d2b7f8fefb278dad036abf2cc9cc89e476709fec
vagifrasulzade <vaqufresulzade@gmail.com>
2026-01-25 16:31:57 +0000

    .

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""first ORM"""
-
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost", user=sys.argv[1],
-        password=sys.argv[2], database=sys.argv[3], port=3306
-        )
+if _name_ == "_main_":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    cs = db.cursor()
-    cs.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cs.fetchall()
-    for row in rows:
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=database
+    )
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for row in cursor.fetchall():
         print(row)
 
-    cs.close()
+    cursor.close()
     db.close()
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-This script lists all states with a name
-starting with N from the database hbtn_0e_0_usa
-"""
+"""first ORM"""
 
 import MySQLdb
 import sys
+
+
 
 
 def connection():
