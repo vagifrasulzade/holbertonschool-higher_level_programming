da0baa2de71d5c11d60db9fa01273fc84aa173fd
unknown <vaqufresulzade@gmail.com>
2026-01-30 16:18:35 +0400

    .

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+function factorial(n) {
+  if (isNaN(n) || n <= 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(num));
