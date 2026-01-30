1f542992e81cd79d2c4c0b769c12aced87472c2d
unknown <vaqufresulzade@gmail.com>
2026-01-30 15:45:48 +0400

    .

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const lines = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
-  
+
 for (const line of lines) {
-    console.log(line);
+  console.log(line);
 }
