42aed56e45a27e50e0d8ed8d01e0ca464e9eaafb
vagifrasulzade <vaqufresulzade@gmail.com>
2026-01-30 12:36:46 +0000

    .

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2).map(x => parseInt(x));
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  const sorted = args.sort((a, b) => b - a);
+  console.log(sorted[1]);
+}
---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+// Function to add two numbers
+function add (a, b) {
+    return a + b;
+}
+
+module.exports.add = add;
+
+  
