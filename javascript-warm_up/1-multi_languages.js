7491992025b768c7ac44420142e49a491e5d02fe
unknown <vaqufresulzade@gmail.com>
2026-01-30 14:56:45 +0400

    .

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// This script prints three lines about different programming languages
+const firstline = 'C is fun';
+const secondline = 'Python is cool';
+const thirdline = 'JavaScript is amazing';
+
+console.log(`${firstline}\n${secondline}\n${thirdline}`);
